911f789467ec4beb692590fb9f9e2ed75851f0cd
Aditya Varun Dhayapulay <93042524+adit24dhaya@users.noreply.github.com>
2024-11-07 12:48:14 -0800

    changes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/z1D9cakf)
+
+#Assignment 3 : Color Maker Submission 
+
+My submission for CPSC 411A-01 18428
+
+#My Information 
+
+* Name: Aditya Varun Dhayapulay 
+* CWID : 845216126
+* Email : adivd@csu.fullerton.edu 
+
+#Instruction
+
+Hey professor, I have tried to fix all the red tagged problems in my code, I might have left a few negligible errors as they were not breaking the code. I wanted to improve the UI by adding some title screen but was not able to figure it out. I wish to make a way better smooth UI in the next assignment.
+
+I have learnt a lot in this assignment and I'm willing to do better in next one, Thank you.
--- app/src/main/java/com/example/color_pallet/MainActivity.kt
+++ app/src/main/java/com/example/color_pallet/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.color_pallet
 
-
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
@@ -23,11 +22,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var editTextGreen: EditText
     private lateinit var editTextBlue: EditText
     private lateinit var resetButton: Button
+    private lateinit var colorCodeTextView: TextView
+
 
     // RGB values
     private var red = 0
     private var green = 0
     private var blue = 0
+
+    private var redBackup = 0
+    private var greenBackup = 0
+    private var blueBackup = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +50,7 @@ class MainActivity : AppCompatActivity() {
         editTextGreen = findViewById(R.id.editTextGreen)
         editTextBlue = findViewById(R.id.editTextBlue)
         resetButton = findViewById(R.id.button)
+        colorCodeTextView = findViewById(R.id.textView)
 
         // Initialize listeners
         setupListeners()
@@ -53,42 +59,95 @@ class MainActivity : AppCompatActivity() {
     private fun setupListeners() {
         // Switch for enabling/disabling Red control
         switchRed.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // Restore the previous red value
+                red = redBackup
+                seekBarRed.progress = red
+                editTextRed.setText(String.format(Locale.US, "%.3f", red / 255.0f))
+            } else {
+                // Save the current red value and set it to 0
+                redBackup = red
+                red = 0
+                seekBarRed.progress = red
+                editTextRed.setText("0.000")
+            }
             seekBarRed.isEnabled = isChecked
+            editTextRed.isEnabled = isChecked
             updateColor()
         }
 
         // Switch for enabling/disabling Green control
         switchGreen.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // Restore the previous green value
+                green = greenBackup
+                seekBarGreen.progress = green
+                editTextGreen.setText(String.format(Locale.US, "%.3f", green / 255.0f))
+            } else {
+                // Save the current green value and set it to 0
+                greenBackup = green
+                green = 0
+                seekBarGreen.progress = green
+                editTextGreen.setText("0.000")
+            }
             seekBarGreen.isEnabled = isChecked
+            editTextGreen.isEnabled = isChecked
             updateColor()
         }
 
         // Switch for enabling/disabling Blue control
         switchBlue.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // Restore the previous blue value
+                blue = blueBackup
+                seekBarBlue.progress = blue
+                editTextBlue.setText(String.format(Locale.US, "%.3f", blue / 255.0f))
+            } else {
+                // Save the current blue value and set it to 0
+                blueBackup = blue
+                blue = 0
+                seekBarBlue.progress = blue
+                editTextBlue.setText("0.000")
+            }
             seekBarBlue.isEnabled = isChecked
+            editTextBlue.isEnabled = isChecked
             updateColor()
         }
 
+
         // SeekBar listener for Red
-                seekBarRed.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-                    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                        red = progress
-                        val redScaled = red / 255.0f
-                        editTextRed.setText(String.format(Locale.US, "%.2f", redScaled))
-                        updateColor()
-                    }
+        seekBarRed.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                red = progress
+                val redScaled = red / 255.0f
+                editTextRed.setText(String.format(Locale.US, "%.3f", redScaled))
+                updateColor()
+            }
 
-                    override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-                    override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-                })
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
 
+        // EditText listener for Red
+        editTextRed.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) { // When the user leaves the EditText
+                val input = editTextRed.text.toString().toFloatOrNull()
+                if (input != null && input in 0.0..1.0) {
+                    red = (input * 255).toInt()
+                    seekBarRed.progress = red
+                    updateColor()
+                } else {
+                    editTextRed.setText(String.format(Locale.US, "%.3f", red / 255.0f))
+                }
+            }
+        }
 
         // SeekBar listener for Green
         seekBarGreen.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 green = progress
                 val greenScaled = green / 255.0f
-                editTextGreen.setText(String.format(Locale.US, "%.2f", greenScaled))  // Specify Locale.US
+                editTextGreen.setText(String.format(Locale.US, "%.3f", greenScaled))
                 updateColor()
             }
 
@@ -96,13 +155,26 @@ class MainActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
 
+        // EditText listener for Green
+        editTextGreen.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) { // When the user leaves the EditText
+                val input = editTextGreen.text.toString().toFloatOrNull()
+                if (input != null && input in 0.0..1.0) {
+                    green = (input * 255).toInt()
+                    seekBarGreen.progress = green
+                    updateColor()
+                } else {
+                    editTextGreen.setText(String.format(Locale.US, "%.3f", green / 255.0f))
+                }
+            }
+        }
 
         // SeekBar listener for Blue
         seekBarBlue.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 blue = progress
                 val blueScaled = blue / 255.0f
-                editTextBlue.setText(String.format(Locale.US, "%.2f", blueScaled))  // Specify Locale.US
+                editTextBlue.setText(String.format(Locale.US, "%.3f", blueScaled))
                 updateColor()
             }
 
@@ -110,6 +182,19 @@ class MainActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
 
+        // EditText listener for Blue
+        editTextBlue.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) { // When the user leaves the EditText
+                val input = editTextBlue.text.toString().toFloatOrNull()
+                if (input != null && input in 0.0..1.0) {
+                    blue = (input * 255).toInt()
+                    seekBarBlue.progress = blue
+                    updateColor()
+                } else {
+                    editTextBlue.setText(String.format(Locale.US, "%.3f", blue / 255.0f))
+                }
+            }
+        }
 
         // Reset button listener to reset RGB values and UI
         resetButton.setOnClickListener {
@@ -131,7 +216,16 @@ class MainActivity : AppCompatActivity() {
 
         // Set the color of the view
         colorView.setBackgroundColor(color)
+
+        // Update the TextView with the hexadecimal color code
+        val hexColor = String.format("#%02X%02X%02X", red, green, blue)
+        colorCodeTextView.text = hexColor
+
+        // Set the background color of the TextView to match the generated color
+        colorCodeTextView.setBackgroundColor(color)
     }
+
+
     // Method to reset all values to 0
     private fun resetValues() {
         seekBarRed.progress = 0
